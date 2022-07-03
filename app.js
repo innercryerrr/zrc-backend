bbe406c2fb83935211ab63cf53e6cdb1aea51df7
@lupasc <108242991+innercryerrr@users.noreply.github.com>
2022-07-03 18:30:25 -0300

    srdf

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(logger('dev'))
 app.use(express.json())
 app.use(cookieParser())
 app.use(express.urlencoded({ extended: false }))
-app.use(express.static(path.join(__dirname, './services/public')))
+app.use(express.static(path.join(__dirname, 'services', 'public', '*')))
 
 // route index
 app.use('/rest/', restRouter)
