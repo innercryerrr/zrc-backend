26775858f3aa52784881f31dbe771dafa616e498
@lupasc <108242991+innercryerrr@users.noreply.github.com>
2022-07-03 19:45:20 -0300

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(logger('dev'))
 app.use(express.json())
 app.use(cookieParser())
 app.use(express.urlencoded({ extended: false }))
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public', '*')))
 
 // route index
 app.use('/rest', restRouter)
